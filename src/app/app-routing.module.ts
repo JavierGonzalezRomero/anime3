1faceda13533e2c66b28a10e521c80e2049586c8
JavierGonzalezRomero <javier.gonzalez@bootcamp-upgrade.com>
2022-11-01 09:55:56 +0100

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllAnimeComponent } from './pages/all-anime/all-anime.component';
+import { FilterComponent } from './pages/filter/filter.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent
+},
+{path:'all',component:AllAnimeComponent
+},
+{path:'filter',component:FilterComponent},
+{path:'filter/:id',component:FilterComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AllAnimeComponent } from './pages/all-anime/all-anime.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FilterComponent } from './pages/filter/filter.component';
+import { GalleryComponent } from './shared/components/gallery/gallery.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    AllAnimeComponent,
+    HomeComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
